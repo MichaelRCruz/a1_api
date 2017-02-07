bdb11dfc909f9d553e7f05ed21939fe99e1d3d92
Michael Cruz <michael.cruz@uwalumni.com>
2017-02-06 18:46:33 -0800

    mounts routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var index = require('./config/routes');
 
 var index = require('./routes/index');
 // var users = require('./routes/users');
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 app.use('/', index);
+app.use('/api/peoples', index);
 // app.use('/users', users);
 
 // catch 404 and forward to error handler
