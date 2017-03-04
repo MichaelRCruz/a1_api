b1ecc8de2cdf96e2e1f3b0f9e5228d8f1d7b317f
Michael Cruz <michael.cruz@uwalumni.com>
2017-03-03 17:31:44 -0800

    adds dotenv

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,16 @@
 var passport = require('passport');
 var GithubStrategy = require('passport-github').Strategy;
-var User = require('../models/user')
+var User = require('../models/user');
+var dotenv = require('dotenv');
+
+dotenv.load();
 
 passport.use(new GithubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_SECRET,
   callbackURL: 'http://localhost:3000/oauth2callback'
-  }, function(accessToken, refreshToken, profile, cb){
+}, function(accessToken, refreshToken, profile, cb){
     User.findOne({ 'github_id': profile.id.toString() }, function(err, user) {
-      // console.log('hi');
       if (err) return cb(err);
       if (user) {
         return cb(null, user);
@@ -17,6 +19,7 @@ passport.use(new GithubStrategy({
       } else {
         // we have a new user via oAuth!
         console.log( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~", profile );
+
         var newUser = new User({
           name: profile.displayName,
           github_user_name: profile.username,
--- config/seeds.js
+++ config/seeds.js
@@ -2,106 +2,106 @@ var mongoose = require('./database');
 var User = require('../models/user');
 
 var users = [
-  // { // 0
-  //   name: "Michael Cruz",
-  //   phone: "(555) 555-5555",
-  //   email: "person@people.com",
-  //   employer: "employer",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // },
-  // { // 1
-  //   name: "Elon Musk",
-  //   phone: "(555) 555-5555",
-  //   email: "elon@tesla.com",
-  //   employer: "Tesla",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // },
-  // { // 2
-  //   name: "Tim Cook",
-  //   phone: "(555) 555-5555",
-  //   email: "tim@apple.com",
-  //   employer: "Apple",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // },
-  // { // 3
-  //   name: "Sundai Pichai",
-  //   phone: "(555) 555-5555",
-  //   email: "sundai@google.com",
-  //   employer: "Google",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // },
-  // { // 4
-  //   name: "Larry Page",
-  //   phone: "(555) 555-5555",
-  //   email: "larry@alphabet.com",
-  //   employer: "Alphabet",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // },
-  // { // 5
-  //   name: "Mark Zuckerberg",
-  //   phone: "(555) 555-5555",
-  //   email: "mark@facebook.com",
-  //   employer: "Facebook",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // },
-  // { // 6
-  //   name: "Jeff Bezos",
-  //   phone: "(555) 555-5555",
-  //   email: "jeff@amazon.com",
-  //   employer: "Amazon",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // },
-  // { // 7
-  //   name: "Satya Nadella",
-  //   phone: "(555) 555-5555",
-  //   email: "satya@microsoft.com",
-  //   employer: "Microsoft",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // },
-  // { // 8
-  //   name: "Marissa Mayer",
-  //   phone: "(555) 555-5555",
-  //   email: "marissa@yahoo.com",
-  //   employer: "Yahoo",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // },
-  // { // 9
-  //   name: "Susan Wojcicki",
-  //   phone: "(555) 555-5555",
-  //   email: "susan@youtube.com",
-  //   employer: "YouTube",
-  //   github: "github",
-  //   linkedin: "linkedin",
-  //   facebook: "facebook",
-  //   projects: {}
-  // }
+  { // 0
+       name: "Michael Cruz",
+       phone: "(555) 555-5555",
+       email: "person@people.com",
+       employer: "employer",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     },
+     { // 1
+       name: "Elon Musk",
+       phone: "(555) 555-5555",
+       email: "elon@tesla.com",
+       employer: "Tesla",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     },
+     { // 2
+       name: "Tim Cook",
+       phone: "(555) 555-5555",
+       email: "tim@apple.com",
+       employer: "Apple",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     },
+     { // 3
+       name: "Sundai Pichai",
+       phone: "(555) 555-5555",
+       email: "sundai@google.com",
+       employer: "Google",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     },
+     { // 4
+       name: "Larry Page",
+       phone: "(555) 555-5555",
+       email: "larry@alphabet.com",
+       employer: "Alphabet",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     },
+     { // 5
+       name: "Mark Zuckerberg",
+       phone: "(555) 555-5555",
+       email: "mark@facebook.com",
+       employer: "Facebook",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     },
+     { // 6
+       name: "Jeff Bezos",
+       phone: "(555) 555-5555",
+       email: "jeff@amazon.com",
+       employer: "Amazon",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     },
+     { // 7
+       name: "Satya Nadella",
+       phone: "(555) 555-5555",
+       email: "satya@microsoft.com",
+       employer: "Microsoft",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     },
+     { // 8
+       name: "Marissa Mayer",
+       phone: "(555) 555-5555",
+       email: "marissa@yahoo.com",
+       employer: "Yahoo",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     },
+     { // 9
+       name: "Susan Wojcicki",
+       phone: "(555) 555-5555",
+       email: "susan@youtube.com",
+       employer: "YouTube",
+       github: "github",
+       linkedin: "linkedin",
+       facebook: "facebook",
+       projects: {}
+     }
 ];
 
 User.remove({}, function(err) {
