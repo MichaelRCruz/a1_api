4408aed12d315da891d9e9a521ec4e60f6813a34
Michael Cruz <michael.cruz@uwalumni.com>
2017-03-09 12:54:05 -0800

    removes commented code

--- controllers/api_login.js
+++ controllers/api_login.js
@@ -2,10 +2,6 @@ module.exports = {
   index: index
 };
 
-// function index(req, res, next) {
-//   req.user._id ? res.json({}) : res.send(403)
-// };
-
 function index(req, res, next) {
   req.user ? res.json({}) : res.send(403)
 };
