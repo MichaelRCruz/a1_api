6b8da7f930999fd30ba74641875faf8023330062
Michael Cruz <michael.cruz@uwalumni.com>
2017-03-09 12:51:36 -0800

    constructs login route

--- controllers/api_login.js
+++ controllers/api_login.js
@@ -1,19 +1,11 @@
-var Post = require('../models/post');
-
-
 module.exports = {
   index: index
-}
+};
+
+// function index(req, res, next) {
+//   req.user._id ? res.json({}) : res.send(403)
+// };
 
 function index(req, res, next) {
-  console.log('sup_yo');
-  console.log(req.user);
-  if (req.user) {
-    Post.find({ created_by: req.user._id }, function(err, posts) {
-      if (err) res.send(err);
-      res.json(posts);
-    });
-  } else {
-    res.send(403);
-  }
+  req.user ? res.json({}) : res.send(403)
 };
