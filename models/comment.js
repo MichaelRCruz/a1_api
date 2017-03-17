f88a36a2b02e3152907ae1a73e4cbb7b749c56d8
Michael Cruz <michael.cruz@uwalumni.com>
2017-03-16 17:07:29 -0700

    modifies comment model for replies

--- models/comment.js
+++ models/comment.js
@@ -4,6 +4,7 @@ var commentSchema = new mongoose.Schema({
   content: String,
   belongs_to: String,
   created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  replies: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment'},
   created: { type: Date, default: Date.now }
 });
 
