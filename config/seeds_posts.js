df673c258ea3212f497e2c766c61b331cdefcdbd
Michael Cruz <michael.cruz@uwalumni.com>
2017-03-17 07:28:32 -0700

    modifies seeds files

--- config/seeds_posts.js
+++ config/seeds_posts.js
@@ -2,7 +2,24 @@ var mongoose = require('./database');
 var Post = require('../models/post');
 
 var posts = [
-  
+  {
+  "_id": "58ca1586c816b58c0c20aaf0",
+  "title": "Man Bun Trust Fund iPhone",
+  "content": "3 wolf moon kogi affogato, trust fund seitan raclette stumptown vaporware try-hard venmo distillery beard locavore hexagon banjo. Try-hard kitsch jianbing, roof party hammock af church-key tumeric raclette. Gentrify tilde occupy narwhal squid. Vaporware kickstarter vape tumblr street art +1. Small batch knausgaard pork belly biodiesel, chia street art deep v wolf seitan mixtape. Distillery plaid health goth, glossier bushwick banh mi man braid tilde pour-over flannel sustainable banjo. Chicharrones salvia activated charcoal, chia scenester sartorial seitan blue bottle copper mug humblebrag venmo viral.",
+  "created_by": {
+  "_id": "58ca1304c816b58c0c20aaee",
+  "name": "Daniel Lamando",
+  "github_user_name": "danopia",
+  "github_profile_url": "https://github.com/danopia",
+  "github_avatar_url": "https://avatars1.githubusercontent.com/u/40628?v=3",
+  "github_id": "40628",
+  "__v": 0,
+  "created": "2017-03-16T04:22:28.578Z"
+  },
+  "topic": "general",
+  "__v": 0,
+  "created": "2017-03-16T04:33:10.233Z"
+  }
 ];
 
 Post.remove({}, function(err) {
--- config/seeds_users.js
+++ config/seeds_users.js
@@ -2,106 +2,16 @@ var mongoose = require('./database');
 var User = require('../models/user');
 
 var users = [
-  { // 0
-       name: "Michael Cruz",
-       phone: "(555) 555-5555",
-       email: "person@people.com",
-       employer: "employer",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     },
-     { // 1
-       name: "Elon Musk",
-       phone: "(555) 555-5555",
-       email: "elon@tesla.com",
-       employer: "Tesla",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     },
-     { // 2
-       name: "Tim Cook",
-       phone: "(555) 555-5555",
-       email: "tim@apple.com",
-       employer: "Apple",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     },
-     { // 3
-       name: "Sundai Pichai",
-       phone: "(555) 555-5555",
-       email: "sundai@google.com",
-       employer: "Google",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     },
-     { // 4
-       name: "Larry Page",
-       phone: "(555) 555-5555",
-       email: "larry@alphabet.com",
-       employer: "Alphabet",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     },
-     { // 5
-       name: "Mark Zuckerberg",
-       phone: "(555) 555-5555",
-       email: "mark@facebook.com",
-       employer: "Facebook",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     },
-     { // 6
-       name: "Jeff Bezos",
-       phone: "(555) 555-5555",
-       email: "jeff@amazon.com",
-       employer: "Amazon",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     },
-     { // 7
-       name: "Satya Nadella",
-       phone: "(555) 555-5555",
-       email: "satya@microsoft.com",
-       employer: "Microsoft",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     },
-     { // 8
-       name: "Marissa Mayer",
-       phone: "(555) 555-5555",
-       email: "marissa@yahoo.com",
-       employer: "Yahoo",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     },
-     { // 9
-       name: "Susan Wojcicki",
-       phone: "(555) 555-5555",
-       email: "susan@youtube.com",
-       employer: "YouTube",
-       github: "github",
-       linkedin: "linkedin",
-       facebook: "facebook",
-       projects: {}
-     }
+  {
+  "_id": "58ca1304c816b58c0c20aaee",
+  "name": "Daniel Lamando",
+  "github_user_name": "danopia",
+  "github_profile_url": "https://github.com/danopia",
+  "github_avatar_url": "https://avatars1.githubusercontent.com/u/40628?v=3",
+  "github_id": "40628",
+  "__v": 0,
+  "created": "2017-03-16T04:22:28.578Z"
+  }
 ];
 
 User.remove({}, function(err) {
